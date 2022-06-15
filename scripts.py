56f3852e3ff3de3ffa9f71122e78c20e231efd6e
art <monkeyking1984@yandex.ru>
2022-06-15 14:05:55 +0300

    edited in accordance with the PEP8

--- scripts.py
+++ scripts.py
@@ -1,7 +1,8 @@
 import random
 
 from django.core.exceptions import MultipleObjectsReturned
-from datacenter.models import Lesson, Schoolkid, Commendation, Mark, Chastisement
+from datacenter.models import Lesson, Schoolkid, Commendation, \
+    Mark, Chastisement
 
 
 class NameError(Exception):
@@ -18,13 +19,15 @@ def get_schoolkid(schoolkid_name):
         message = f'Имя  "{schoolkid_name}" не найдено в БД. Введите другое.'
         raise NameError(message)
     except MultipleObjectsReturned:
-        message = f'Для имени "{schoolkid_name}" найдено несколько вариантов. Конкретизируйте запрос.'
+        message = f'Для имени "{schoolkid_name}" ' \
+                  f'найдено несколько вариантов. Конкретизируйте запрос.'
         raise NameError(message)
     return child
 
 
-def fix_marks(schoolkid, new_mark, bad_mark):
-    Mark.objects.filter(schoolkid=schoolkid, points__in=bad_mark).update(points=new_mark)
+def fix_marks(schoolkid, new_mark, bad_marks):
+    Mark.objects.filter(
+        schoolkid=schoolkid, points__in=bad_marks).update(points=new_mark)
 
 
 def remove_chastisements(schoolkid):
@@ -32,34 +35,37 @@ def remove_chastisements(schoolkid):
     child_chastisement.delete()
 
 
-def lesson_without_commendation(schoolkid, lessons):
+def get_lesson_without_commendation(schoolkid, lessons):
     parallel_lessons = lessons.filter(
         year_of_study__contains=schoolkid.year_of_study,
-        group_letter__contains =schoolkid.group_letter
+        group_letter__contains=schoolkid.group_letter
     )
 
     child_commendation = Commendation.objects.filter(schoolkid=schoolkid)
 
     for parallel_lesson in parallel_lessons.order_by('-date'):
-        if not child_commendation.filter(created=parallel_lesson.date).exists():
+        if not child_commendation.filter(
+                created=parallel_lesson.date).exists():
             return parallel_lesson
 
 
 def create_commendation(schoolkid, lesson_name):
-    commendation_texts = ['Молодец!',
-                          'Ты меня очень обрадовал!',
-                          'Великолепно!',
-                          ]
+    commendation_texts = [
+        'Молодец!',
+        'Ты меня очень обрадовал!',
+        'Великолепно!',
+    ]
 
     commendation_text = random.choice(commendation_texts)
 
     lessons = Lesson.objects.filter(subject__title__contains=lesson_name)
 
     if not lessons.exists():
-        message = f'Предмет "{lesson_name}" не найден. Введите другое название предмета.'
+        message = f'Предмет "{lesson_name}" не найден. ' \
+                  f'Введите другое название предмета.'
         raise NameError(message)
 
-    commendation_lesson = lesson_without_commendation(schoolkid, lessons)
+    commendation_lesson = get_lesson_without_commendation(schoolkid, lessons)
     Commendation.objects.create(
         created=commendation_lesson.date,
         schoolkid=schoolkid,
@@ -69,7 +75,9 @@ def create_commendation(schoolkid, lesson_name):
     )
 
 
-def improve_school_results(schoolkid_name, lesson_name, new_mark=5, bad_marks=[2, 3]):
+def improve_school_results(
+        schoolkid_name, lesson_name, new_mark=5,
+        bad_marks=[2, 3]):
 
     if not lesson_name:
         print('Название предмета не введено. Повторите ввод.')
